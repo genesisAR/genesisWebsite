e924dc1d9ca7fe8d4db7f386e85fd9c4e8996ed5
genesisAR <long@genesisar.co>
2016-08-03 13:16:51 +1000

    Signup form fix

--- signup.php
+++ signup.php
@@ -3,6 +3,7 @@
 <style type="text/css">
 	#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
   #mc_embed_signup form {padding-left: 0;}
+  #mce-responses {margin-left: 0; padding-left: 0;}
 	/* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
 	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
 </style>
@@ -33,5 +34,6 @@
     </div>
 </form>
 </div>
+<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
 
 <!--End mc_embed_signup-->
